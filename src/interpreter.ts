6418773a1ebdb710b787feeda20c9c3a1d9570ca
Slava Akhmechet <coffeemug@gmail.com>
2025-12-21 18:30:10 -0500

    adding support for symbol evaluation

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,15 +1,22 @@
 import type { Expr } from "./parser";
 import type { RuntimeObj } from "./runtime_objs";
 import { makeIntObj } from "./runtime_objs/int";
-import type { Env } from "./env";
+import { intern, type Env } from "./env";
 
 export function evaluate(expr: Expr, env: Env): RuntimeObj {
   switch (expr.type) {
     case 'int':
       return makeIntObj(expr.value, env);
+    case 'ident': {
+      const sym = intern(env, expr.name);
+      if (!sym.value) {
+        throw new Error(`Unbound symbol ${print(sym)}`);
+      }
+      return sym.value;
+    }
   }
 
-  const _exhaustive: never = expr.type;
+  const _exhaustive: never = expr;
 }
 
 export function print(obj: RuntimeObj): string {
--- src/parser.ts
+++ src/parser.ts
@@ -1,10 +1,17 @@
-import { int, eof, seq } from "@spakhm/ts-parsec";
+import { int, eof, seq, either, alpha, alnum, many, lex } from "@spakhm/ts-parsec";
 import { fromString } from "@spakhm/ts-parsec";
 
-export type Expr = { type: "int"; value: number };
+export type Expr =
+  | { type: "int"; value: number }
+  | { type: "ident"; name: string };
 
-const expr = seq(int, eof).map(([n, _]) =>
+const ident = lex(seq(alpha, many(alnum))).map(([first, rest]) =>
+  ({ type: "ident" as const, name: [first, ...rest].join("") }));
+
+const intLit = int.map((n) =>
   ({ type: "int" as const, value: n }));
+
+const expr = seq(either(intLit, ident), eof).map(([e, _]) => e);
 
 export function parse(input: string): Expr {
   const stream = fromString(input);
