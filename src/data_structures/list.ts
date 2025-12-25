94272776b06806fae62f6783ffab250e123cefb9
Slava Akhmechet <coffeemug@gmail.com>
2025-12-25 13:58:24 -0500

    Adding list/at support

--- src/data_structures/list.ts
+++ src/data_structures/list.ts
@@ -3,6 +3,7 @@ import { type RootTypeObj } from "../core_objects/root_type"
 import type { RuntimeObj } from "../runtime_objects";
 import { defineBinding } from "../runtime/scope";
 import type { BeepKernel } from "../bootstrap/kernel";
+import type { IntObj } from "./int";
 
 export type ListTypeObj =
   & RuntimeObjMixin<'ListTypeObj', RootTypeObj>
@@ -41,53 +42,46 @@ export function initListMethods(k: BeepKernel) {
 
   const defMethod = makeDefNative<ListObj>(k.sysModule.toplevelScope, listTypeObj)
 
-  // show
   defMethod('show', 0, thisObj => {
     const items = thisObj.elements.map(e => show(e)).join(', ');
     return makeStringObj(`[${items}]`);
   });
 
-  // push - returns new list with element added at end
   defMethod('push', 1, (thisObj, args) =>
     makeListObj([...thisObj.elements, args[0]]));
 
-  // push! - mutates list, adds element at end
   defMethod('push!', 1, (thisObj, args) => {
     thisObj.elements.push(args[0]);
     return thisObj;
   });
 
-  // pop - returns new list with last element removed
   defMethod('pop', 0, thisObj =>
     makeListObj(thisObj.elements.slice(0, -1)));
 
-  // pop! - mutates list, removes last element
   defMethod('pop!', 0, thisObj => {
     thisObj.elements.pop();
     return thisObj;
   });
 
-  // push_front - returns new list with element added at front
   defMethod('push_front', 1, (thisObj, args) =>
     makeListObj([args[0], ...thisObj.elements]));
 
-  // push_front! - mutates list, adds element at front
   defMethod('push_front!', 1, (thisObj, args) => {
     thisObj.elements.unshift(args[0]);
     return thisObj;
   });
 
-  // pop_front - returns new list with first element removed
   defMethod('pop_front', 0, thisObj =>
     makeListObj(thisObj.elements.slice(1)));
 
-  // pop_front! - mutates list, removes first element
   defMethod('pop_front!', 0, thisObj => {
     thisObj.elements.shift();
     return thisObj;
   });
 
-  // len - returns number of elements
   defMethod('len', 0, thisObj =>
     makeIntObj(thisObj.elements.length));
+
+  defMethod('at', 1, (thisObj, args) =>
+    thisObj.elements[(args[0] as IntObj).value]);
 }
